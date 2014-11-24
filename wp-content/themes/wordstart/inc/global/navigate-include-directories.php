a576c1b1ef828240ba50a7d32dbbe9aa89b270a3
jtuds <james@jtudsbury.com>
2014-11-24 16:43:35 +0000

    Delted non theme files

--- wp-content/themes/wordstart/inc/global/navigate-include-directories.php
+++
@@ -1,6 +0,0 @@
-<?php 
-/**
-* Navigate to a required file directory from current location
-* The example below example goes up one directory relative to the current location
-*/
-// include ( dirname(__FILE__).'/../example.php'); ?>
