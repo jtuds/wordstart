29924224c88ba67855f967165297c2a0f350e266
jtuds <james@jtudsbury.com>
2014-11-24 16:36:15 +0000

    Local config

--- local-config.php
+++ local-config.php
@@ -1,10 +1,14 @@
 <?php
 /**
  * Variable Wordpress configurations as included by wp-config.php
- */ 
+ */
+
+define('PROJECT_NAME', 'wordstart');
+$project_name = PROJECT_NAME;
+
 $environments = array(
     'local_james' => 'localhost',
-    'local_steve' => 'spiraltech.dev',
+    'local_steve' => $project_name . '.dev',
     'test' => 'test.',
     'stage' => 'stage.',
     'live' => 'domain.com'
@@ -25,13 +29,13 @@ if( ! defined('ENVIRONMENT') ) define( 'ENVIRONMENT', 'live' );
 // Define different DB connection details depending on environment
 switch( ENVIRONMENT ) {
     case 'local_james':
-        define( 'DB_NAME', 'dbname' );
+        define( 'DB_NAME', $project_name );
         define( 'DB_USER', 'root' );
         define( 'DB_PASSWORD', '' );
         define( 'DB_HOST', 'localhost' );
         define( 'WP_DEBUG', true );
-        define( 'WP_SITEURL', 'http://localhost/projectname' );
-        define( 'WP_HOME', 'http://localhost/projectname' );
+        define( 'WP_SITEURL', 'http://localhost/' . $project_name );
+        define( 'WP_HOME', 'http://localhost/' . $project_name );
         
         define ('JETPACK_DEV_DEBUG', true);
         
@@ -43,8 +47,8 @@ switch( ENVIRONMENT ) {
         define( 'DB_PASSWORD', 'root' );
         define( 'DB_HOST', 'localhost' );
         define( 'WP_DEBUG', true );
-        define( 'WP_SITEURL', 'http://projectname.dev' );
-        define( 'WP_HOME', 'http://projectname.dev' );
+        define( 'WP_SITEURL', 'http://' . $project_name . '.dev' );
+        define( 'WP_HOME', 'http://' . $project_name . '.dev' );
         
         define ('JETPACK_DEV_DEBUG', true);
         
@@ -52,12 +56,12 @@ switch( ENVIRONMENT ) {
  
     case 'test':
  
-        define( 'DB_NAME', 'cstest_dbname' );
+        define( 'DB_NAME', 'cstest_dbname'  );
         define( 'DB_USER', 'cstest_dbuser' );
         define( 'DB_PASSWORD', '9Billion' );
         define( 'DB_HOST', 'localhost' );
-        define( 'WP_SITEURL', 'http://test.candidsky.com/projectname' );
-        define( 'WP_HOME', 'http://test.candidsky.com/projectname' );
+        define( 'WP_SITEURL', 'http://test.candidsky.com/' . $project_name );
+        define( 'WP_HOME', 'http://test.candidsky.com/' . $project_name );
     
     break;
 }
